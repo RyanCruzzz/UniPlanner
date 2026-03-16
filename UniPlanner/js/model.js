ea97680aa85e48c2bdb3243e4a0048876ea63ec1
Ryan <ryanaprediz2023@gmail.com>
2026-03-16 17:59:46 -0300

    Sprint 2: modelagem de dados

---
+++ UniPlanner/js/model.js
@@ -0,0 +1,26 @@
+// É aqui que vou definir como as informações serão estruturadas
+// Lembrando que cada disciplina e cada tarefa vai ser um objeto com suas propriedades
+const COLORS = ['#7c6ffc',  '#fc6f9e',  '#6ffcb7',  '#fcd76f',  '#6fb8fc',  '#fc9b6f'] // ponto importante, o nome 'COLORS' é uma convenção, ou seja, (indica que é uma constante global). Que nunca muda. E uma convenção seria um acordo entre os desenvolvedores, isso não é uma regrada linguagem
+
+let subjects = [ //vai armazenar todas as disciplinas, ela vai começar vazia
+
+]
+
+let tasks = [ // armazena todas as tarefas, começa vazia também
+
+]
+
+let completedTasks = [ //armazena todas as tarefas concluidas, começa vazia
+
+]
+
+// VARIÁVEIS DE CONTROLE - elas controlam o estado da interface
+let currentSubjectId = null
+let focusedTask = null
+let selectedColor = COLORS[0]
+
+
+
+
+
+
